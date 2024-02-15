133f9e601de2c19aae935c13042fc607be5df15b
Doru Irimescu <>
2024-02-15 14:05:01 +0200

    Add wip

--- script.js
+++ script.js
@@ -1,59 +1,64 @@
 let currentFlashcardIndex = 0;
+
 const flashcards = [
     {
       title: 'SULAA (2)',
       frontImage: 'image17.png',
       backImage: 'assets/back/image41.png',
-      content: `
-    <div class="flashcard-header">
-        <h2>Lumi sulaa, kun aurinko paistaa.</h2>
-    </div>
-    <div class="flashcard-list">
-        <ol>
-            <li>First shit whatever bullshit lol what</li>
-            <li>Second shit whatever bullshit lel</li>
-        </ol>
-    </div>`
+      frontContent:"The snow melts when the sun is shining",
+      sentence: "Lumi sulaa, kun aurinko paistaa.",
+      listContent:[
+        "sulan / sulin / olen sulanut / olin sulanut",
+        "sulaisin / olisin sulanut / sulettaisiin, ei sulettaisi",
+        "<a href='https://www.verbix.com/webverbix/finnish/sulaa'>Verbix</a>"
+      ]
     },
     {
       title: 'SULATTAA (2)',
       frontImage: 'a2.png',
       backImage: 'assets/back/image29.png',
-      content: `
-      <div class="flashcard-header">
-        <h2>Aurinko sulttaa lumen.</h2>
-      </div>
-      <div class="flashcard-list">
-        <ol>
-            <li>Sulattaa + obj</li>
-            <li>Second</li>
-        </ol>
-    </div>
-    `
+      frontContent:"The sun melts the snow",
+      sentence: "Aurinko sulttaa lumen.",
+      listContent:[
+        "Sulattaa + obj",
+        "<a href='https://www.verbix.com/webverbix/finnish/sulttaa'>Verbix</a>"
+      ]
     },
-  // Add more flashcards as needed
 ];
 
-function displayFlashcard(index) {
-  const flashcard = flashcards[index];
-  const flashcardContainer = document.getElementById('flashcardContainer');
-  flashcardContainer.innerHTML = `
-    <div class="flashcard" onclick="this.classList.toggle('flipped')">
-      <div class="front" id="front_flash">
-      </div>
-      <div class="back" id="back_flash">
-        <div class="back-content">
-            <h2 class="flashcard-title">${flashcard.title}</h2>
-            ${flashcard.content}
-            <!-- Render HTML content -->
-        </div>
-      </div>
-    </div>
-  `;
-  document.getElementById('front_flash').style.backgroundImage = `url(${flashcard.frontImage})`;
-  document.getElementById('back_flash').style.backgroundImage = `url(${flashcard.backImage})`;
-
+function updateStats(currentIndex, totalCards) {
+    const percentage = (currentIndex / totalCards) * 100;
+    document.querySelector('.progress-bar').style.width = `${percentage}%`;
+    document.getElementById('cardStats').textContent = `Cards visited: ${currentIndex} / ${totalCards}`;
 }
+
+function displayFlashcard(index) {
+    const flashcard = flashcards[index];
+    const flashcardContainer = document.getElementById('flashcardContainer');
+    flashcardContainer.innerHTML = `
+    <div class="flashcard" onclick="this.classList.toggle('flipped')">
+        <div class="front" id="frontFlash"></div>
+
+        <div class="back" id="backFlash">
+            <div class="back-content">
+                <h2 class="flashcard-title">${flashcard.title}</h2>
+                <div class="flashcard-header">
+                    <h2>${flashcard.sentence}</h2>
+                </div>
+                <div class="flashcard-list">
+                <ul>${flashcard.listContent.map((item) => `<li>${item}</li>`).join('')}</ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    document.getElementById('frontFlash').style.backgroundImage = `url(${flashcard.frontImage})`;
+    document.getElementById('backFlash').style.backgroundImage = `url(${flashcard.backImage})`;
+    document.querySelector('.front').style.setProperty('--bubble-content', `"${flashcard.frontContent}"`);
+
+    updateStats(currentFlashcardIndex + 1, flashcards.length);
+}
+
 
 // Initialize with the first flashcard
 displayFlashcard(currentFlashcardIndex);
